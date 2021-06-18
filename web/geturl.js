645d74f9bb2e581bbaab2986200bb6c37058a951
icanwallet <ruitaoliu@qq.com>
2021-06-18 18:32:10 +0800

    modify uni request

--- web/geturl.js
+++ web/geturl.js
@@ -1,48 +1,87 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-import { arrayify } from "../bytes";
-export function getUrl(href, options) {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (options == null) {
-            options = {};
-        }
-        const request = {
-            method: (options.method || "GET"),
-            headers: (options.headers || {}),
-            body: (options.body || undefined),
-            mode: "cors",
-            cache: "no-cache",
-            credentials: "same-origin",
-            redirect: "follow",
-            referrer: "client", // no-referrer, *client
-        };
-        const response = yield fetch(href, request);
-        const body = yield response.arrayBuffer();
-        const headers = {};
-        if (response.headers.forEach) {
-            response.headers.forEach((value, key) => {
-                headers[key.toLowerCase()] = value;
-            });
-        }
-        else {
-            ((response.headers).keys)().forEach((key) => {
-                headers[key.toLowerCase()] = response.headers.get(key);
-            });
-        }
-        return {
-            headers: headers,
-            statusCode: response.status,
-            statusMessage: response.statusText,
-            body: arrayify(new Uint8Array(body)),
-        };
-    });
+import {
+	arrayify
+} from "../bytes";
+import { toUtf8Bytes} from "../strings";
+
+
+export async function getUrl(href, options) {
+	if (options == null) {
+		options = {};
+	}
+	if(typeof fetch === 'function' && typeof plus == 'undefined'){
+		return await getFetch(href, options)
+	}else{
+		return await getUni(href, options)
+	}
 }
-//# sourceMappingURL=geturl.js.map
+
+async function getUni(href, options) {
+	
+	if (options.body) {
+		let str = String.fromCharCode.apply(null, options.body);
+		//let str = ab2str(options.body)
+		options.body = JSON.parse(str);
+	} else {
+		options.body = {};
+	}
+	let request = {
+		url: href,
+		method: (options.method || "GET"),
+		//header: (options.headers || {}),
+		header: {
+			'content-type' : 'application/json; charset=utf-8'
+		},
+		data: options.body,
+		mode: "cors"
+	};
+	//console.log(request);
+	let [err, rt] = await uni.request(request);
+	//console.log(rt)
+	//let body = rt.data
+	let str = JSON.stringify(rt.data)
+	let body = toUtf8Bytes(str)
+	let status = rt.statusCode;
+	let statusText = "Ok"
+	let headers = {};
+	let rtheader = rt.header;
+	//console.log(rtheader);
+	for (let p in rtheader) {
+		headers[p.toLowerCase()] = rtheader[p];
+	}
+	return {
+		headers: headers,
+		statusCode: status,
+		statusMessage: statusText,
+		body: body
+	};
+}
+async function getFetch(href, options) {
+	let request = {
+		method: (options.method || "GET"),
+		headers: (options.headers || {}),
+		body: (options.body || undefined),
+		mode: "cors",
+		cache: "no-cache",
+		credentials: "same-origin",
+		redirect: "follow",
+		referrer: "client", // no-referrer, *client
+	};
+	let response = await fetch(href, request);
+	let body = await response.arrayBuffer();
+	let headers = {};
+	if (response.headers.forEach) {
+		response.headers.forEach((value, key) => {
+			headers[key.toLowerCase()] = value;
+		});
+	} else {
+		((response.headers).keys)().forEach((key) => {
+			headers[key.toLowerCase()] = response.headers.get(key);
+		});
+	}
+	return {
+		headers: headers,
+		statusCode: response.status,
+		statusMessage: response.statusText,
+		body: arrayify(new Uint8Array(body)),
+	};
+}
